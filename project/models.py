d85988e76c0eff6c4637a633c5eaee4788cfb051
Subhodeep <subhodeep.s2013@gmail.com>
2020-11-16 13:20:18 +0530

    Created 'project' model
    Added the title,description and image upload field using Django fields.

--- project/models.py
+++ project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#models are the objects that will be put into the database through admin panel
 
 # Create your models here.
+class Project(models.Model): #We inherit Model class from model
+    title = models.CharField(max_length=200) #CharField class will provide a Java like TextField in the admin panel
+    description = models.CharField(max_length=300) #max_length parameter will limit the maximum length of the text entered
+    image = models.ImageField(upload_to='images/') #ImageField will let you upload images and will validate it and store it in the specified folder
+    url = models.URLField(blank=True) #URLField will let you write URL
+    
