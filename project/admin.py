f1eb09214831266c292f7b6a1cf75e2db7ca443c
Subhodeep <subhodeep.s2013@gmail.com>
2020-11-18 14:25:03 +0530

    Added superuser and registered 'Project' class
    I created superuser with username: devildeep, email: subhodeep.s2013@gmail.com, password: rootroot
    After this, I imported 'Project' class from project/models.py and registered it with admin.
    Now it is showing up in the admin panel of Django.

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Project #I import the Project class from models.py inside project folder
 # Register your models here.
+admin.site.register(Project) #I register the project class with admin, and now it will show up in the admin panel
