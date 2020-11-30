b3da46d75d7ea6d528c4cac3365e96144c4f7635
Subhodeep <subhodeep.s2013@gmail.com>
2020-11-30 23:28:46 +0530

    Upgraded home page.
    I created 3 more objects and uploaded their respective images.
    Modified the home() in Project/views.py to retrieve all the Project objects from the
    database and pass it to Project/Templetes/Project/home.html.
    Finally added necessary logic to display all the details for every Project object in the
    list passed from home().
    This was just for demo and will be changed.

--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Project #importing the Project class from models.py
 # Create your views here.
 def home(request):
-    return render(request,'project/home.html')
+    projects = Project.objects.all() #list of all the project that are created and stored inside the database   
+    return render(request,'project/home.html',{'projects':projects }) #passed the list of projects as a dictionary
