4ad75cb110077dcb1e9b1b357876793c4cce563f
Subhodeep <subhodeep.s2013@gmail.com>
2020-12-17 23:34:21 +0530

    Added page for individual blogs and enabled links in the blog home page.
    Created a new path for individual blog object, a new function detail() in blog/views.py which will render detail.html which will display the contents.
    Enabled links in the all_blogs.html page. You can hit the title of a blog to visit the individual page for each.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.all_blogs,name="all_blogs")
+    path('',views.all_blogs,name="all_blogs"),
+    path('<int:blog_id>/',views.detail,name="detail") #<int:blog_id> means we enter a 'int' object which is reffered as 'blog_id'
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404 #get_object_or_404() gets the required object and if not found, gives 404 error
 from .models import blog
 # Create your views here.
 
 def all_blogs(request):
     blogs = blog.objects.all()
     return render(request,'blog/all_blogs.html',{'blogs': blogs})
+
+def detail(request,blog_id):
+    #The first argument is the class blog and second is the primary key of the object
+    blog_obj = get_object_or_404(blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':blog_obj})
