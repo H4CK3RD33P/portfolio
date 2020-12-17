6f07fbbbc31bff35f032a590b3fb9efa007290e2
Subhodeep <subhodeep.s2013@gmail.com>
2020-12-17 19:30:51 +0530

    Displayed blog objects on blog/ and added static files.
    Created and registered blog model. Made the changes in databases. Displayed it on blog/ home page.
    Added static files (My image in project/static/images/) and displayed it on home page.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    description = models.TextField()
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import blog
 # Create your views here.
 
 def all_blogs(request):
-    return render(request,'blog/all_blogs.html')
+    blogs = blog.objects.all()
+    return render(request,'blog/all_blogs.html',{'blogs': blogs})
